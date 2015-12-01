35715d10a7012852de3d918fcb6d74681d8305ae
John Towery <kbaries@gmail.com>
2015-12-01 21:42:51 +0000

    L3 Build Model Test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+
+	test "unique_tag" do
+		quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
+		expected = 'KM#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
+
+
 end
